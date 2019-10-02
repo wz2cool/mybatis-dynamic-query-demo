ebfd3ad099dd74eeb8bf46de01716eae9df58b6d
wz2cool <wz2cool@live.cn>
2019-10-02 22:29:16 +0800

    add

---
+++ src/main/java/com/github/wz2cool/mybatisdynamicquerydemo/MybatisDynamicQueryDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.wz2cool.mybatisdynamicquerydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDynamicQueryDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisDynamicQueryDemoApplication.class, args);
+    }
+
+}
